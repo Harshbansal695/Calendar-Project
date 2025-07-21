4d6ef52d09b36c895c92ad8374b85e0450258213
Harsh Bansal <harshbansal695@gmail.com>
2025-07-21 10:44:32 +0530

    app.js start working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Calendar from "./components/Calendar";
+import EventForm from "./components/EventForm";
+import "./styles.css";
 
 function App() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [showEventForm, setShowEventForm] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Event Calendar</h1>
+      <Calendar
+        currentDate={currentDate}
+        setCurrentDate={setCurrentDate}
+        setShowEventForm={setShowEventForm}
+        setSelectedEvent={setSelectedEvent}
+        setSelectedDate={setSelectedDate}
+        selectedEvent={selectedEvent}
+      />
+
+      {showEventForm && (
+        <EventForm
+          selectedDate={selectedDate}
+          selectedEvent={selectedEvent}
+          setShowEventForm={setShowEventForm}
+          currentDate={currentDate}
+        />
+      )}
     </div>
   );
 }
